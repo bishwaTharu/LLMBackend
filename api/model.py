a21ecb42d59d995f3a3b5768eb12d5f9c17eb791
bishwaTharu <bishwa@LAPTOP-FBU8FSB3>
2023-06-17 15:02:20 +0545

    formatted

--- api/model.py
+++ api/model.py
@@ -1,15 +1,18 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-import logging 
+import logging
 import os
 from dotenv import load_dotenv, find_dotenv
-load_dotenv(find_dotenv());
-api_token= os.getenv('HUGGINGFACEHUB_API_TOKEN')
+
+load_dotenv(find_dotenv())
+api_token = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 
 from huggingface_hub import login
+
 login(token=api_token)
 print("loggedIn ...")
 
-class LLM():
+
+class LLM:
     def __init__(self, model, tokenizer):
         self.tokenizer = tokenizer
         self.model = model
@@ -17,13 +20,15 @@ class LLM():
     @classmethod
     def load_model(cls):
         tokenizer = AutoTokenizer.from_pretrained("facebook/blenderbot-400M-distill")
-        model = AutoModelForSeq2SeqLM.from_pretrained("facebook/blenderbot-400M-distill")
+        model = AutoModelForSeq2SeqLM.from_pretrained(
+            "facebook/blenderbot-400M-distill"
+        )
         return cls(model, tokenizer)
 
     def generate_response(self, input_text):
         input = self.tokenizer(input_text, return_tensors="pt")
         outputs = self.model.generate(
-            input['input_ids'],
+            input["input_ids"],
             temperature=0.8,
         )
         logging.info("Output...")
